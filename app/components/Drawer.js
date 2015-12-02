2cefdb6683749dde3bf1235dc910c03e3872eb1e
Jia Rong <jr.loves.maths@gmail.com>
2015-12-02 22:06:11 +0800

    Improve drawer touch areas.

--- app/components/Drawer.js
+++ app/components/Drawer.js
@@ -14,14 +14,16 @@ export default class CustomDrawer extends Component {
           closeDrawer={this.props.closeDrawer}
           />}
           tapToClose={true}
-          openDrawerOffset={.25} // 20% gap on the right side of drawer
-          panCloseMask={.5}
-          closedDrawerOffset={-3}
+          openDrawerOffset={.25}
+          panOpenMask={.1}
+          panCloseMask={1}
+          negotiatePan={true}
           styles={{
-            drawer: {shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
-            main: {paddingLeft: 3}
+            drawer: {borderRightWidth: 1, borderRightColor: '#eee', shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
           }}
-          tweenHandler={(ratio) => ({main: { opacity: (1.2-ratio/(1.2)) }})}
+          tweenHandler={(ratio) =>
+            ({main: { opacity: (1.2-ratio/(1.2)) }})
+          }
           >
           {this.props.main}
         </Drawer>
